2bed208ae6c6ef758b0d385bdc511846a8ad6f21
salinas <80262440+eoSalinas@users.noreply.github.com>
2026-06-21 19:10:16 -0300

    feat(lualine): add relative path to filename

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -27,7 +27,7 @@ return {
                 sections = {
                     lualine_a = { "mode" },
                     lualine_b = { "diagnostics" },
-                    lualine_c = { "filename" },
+                    lualine_c = { { "filename", path = 1 } },
                     lualine_x = { "encoding" },
                     lualine_y = { { "branch", icon = "" } },
                     lualine_z = {},
@@ -35,7 +35,7 @@ return {
                 inactive_sections = {
                     lualine_a = {},
                     lualine_b = {},
-                    lualine_c = { "filename" },
+                    lualine_c = { { "filename", path = 1 } },
                     lualine_x = {},
                     lualine_y = {},
                     lualine_z = {},
