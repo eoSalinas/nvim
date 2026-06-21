9f5ac630619d7105579caa913a8413e1948fcec2
salinas <80262440+eoSalinas@users.noreply.github.com>
2026-06-21 19:20:56 -0300

    feat(remap): add leader q to quit

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,7 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
