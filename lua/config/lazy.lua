ac21c6f9d569d83995b91555b2a3f9b171134b39
salinas <80262440+eoSalinas@users.noreply.github.com>
2026-06-25 11:57:07 -0300

    chore(lazy): silence update checker notification

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,7 +33,7 @@ require("lazy").setup({
     -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "rose-pine" } },
     -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = { enabled = true, notify = false },
 })
 
 require("config.remap")
